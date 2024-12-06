79477815236723d235cb6ae1d59e6becde6c2f5a
StackSamuraiK <150704978+StackSamuraiK@users.noreply.github.com>
2024-12-06 07:42:04 +0530

    Updated db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 // backend/db.js
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://kshitizsrivastav3344:BVlkbEPMqeOL2wQK@cluster0.rnnttmu.mongodb.net/")
+mongoose.connect("your_mongodb_url")
 
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
